50c247bae0abb73bff4025a36abad4e7befe7985
Ebin Louis <ebinlouis1559@gmail.com>
2024-07-04 13:45:44 +0530

    navbar completed

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ document
     .getElementById('volunteer')
     .addEventListener('click', navigateToVolunteer);
 document.getElementById('donors').addEventListener('click', navigateToDonors);
+
+document.getElementById('hamburger-menu').addEventListener('click', () => {
+    // alert('Hello');
+});
+
+// Testing ok..
+
+document
+    .getElementById('hamburger-menu')
+    .addEventListener('click', function () {
+        // alert('heyy');
+        const menu = document.querySelector('.menu');
+        menu.classList.toggle('show');
+    });
